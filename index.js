525d747dbbd12cb9c3796f0a941164f10575ad35
Evan hameed <evanhameed9966@gmail.com>
2020-05-15 13:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,26 +2,43 @@
 function wakeDog(dogName,dogBreed){
 
 console.log(`Wake ${dogName} the ${dogBreed}`);
+let message = `Wake ${dogName} the ${dogBreed}`;
+return message;
 }
 function leashDog(dogName,dogBreed){
 
 console.log(`Leash ${dogName} the ${dogBreed}`);
+let message = `Leash ${dogName} the ${dogBreed}`;
+return message;
+
 }
 function walkToPark(dogName,dogBreed){
 
 console.log(`Walk to park with ${dogName} the ${dogBreed}`);
+let message = `Walk to park with ${dogName} the ${dogBreed}`;
+return message;
+
 }
 function throwFrisbee(dogName,dogBreed){
 
 console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+ let message=`Throw the frisbee for ${dogName} the ${dogBreed}`;
+ return message;
+
 }
 function walkHome(dogName,dogBreed){
 
 console.log(`Walk home with ${dogName} the ${dogBreed}`);
+let message = `Walk home with ${dogName} the ${dogBreed}`;
+return message;
+
 }
 function unleashDog(dogName,dogBreed){
 
 console.log(`Unleash ${dogName} the ${dogBreed}`);
+let message=`Unleash ${dogName} the ${dogBreed}`;
+return message;
+
 }
 
 let routine = [wakeDog(),leashDog(),walkToPark(),throwFrisbee(),walkHome(),unleashDog()] ;
